d5f5534cb7b458c2cfaf3897eb9b5980b112504d
Hugolino B. Guimarães <41154353+hugofficial@users.noreply.github.com>
2022-02-16 13:44:13 -0300

    Update index.js

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -3,12 +3,14 @@ import "./error.css";
 
 export default function Error() {
   return (
+       <>
     <div className="container-error">
       <img src="../image/notfound.png" alt="Pagina nao encontrada" />
       <h1>Pagina não encontrada!</h1>
       <Link to="/">Voltar para Home</Link>
     
     </div>
-   <> <footer class="footer">Hugolino B. G. 2022. Todos os direitos reservados.</footer></>
+ <footer class="footer">Hugolino B. G. 2022. Todos os direitos reservados.</footer>
+    </>
   );
 }
